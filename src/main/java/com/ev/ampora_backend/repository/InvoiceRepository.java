7d3dd70aab8c16a0f9d4981b33bb332c38f8c8f8
Numidu <dnumidu@gmail.com>
2025-11-19 14:04:28 +0530

    complete invoice crud operation

--- src/main/java/com/ev/ampora_backend/repository/InvoiceRepository.java
+++ src/main/java/com/ev/ampora_backend/repository/InvoiceRepository.java
@@ -1,4 +1,8 @@
 package com.ev.ampora_backend.repository;
 
-public class InvoiceRepository {
+import com.ev.ampora_backend.entity.Invoice;
+import com.ev.ampora_backend.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice,String> {
 }
