5de3908137463a8713b19b0b95c73ddf56dd03e6
Maheshi Wickramarathna <maheshiwickramarathna2001@gmail.com>
2025-12-03 23:41:16 +0530

    create api/message and test api through postman

---
+++ src/main/java/com/ev/ampora_backend/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.ev.ampora_backend.controller;
+
+import com.ev.ampora_backend.dto.MessageRequestDto;
+import com.ev.ampora_backend.dto.MessageResponseDto;
+import com.ev.ampora_backend.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/message")
+@RequiredArgsConstructor
+public class MessageController {
+    private  final MessageService messageService;
+
+    @PostMapping
+    public ResponseEntity<MessageResponseDto> createMessage(@RequestBody MessageRequestDto requestDto){
+        return new ResponseEntity<>(messageService.sendMessage(requestDto), HttpStatus.CREATED);
+    }
+
+}
