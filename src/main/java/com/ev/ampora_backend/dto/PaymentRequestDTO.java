d1a7e758f1aeb161b4ebe52aa5f515405b8dd0ef
mari75a <sangeethlakshan0@gmail.com>
2025-12-18 21:19:52 +0530

    Payment DTO

--- src/main/java/com/ev/ampora_backend/dto/PaymentRequestDTO.java
+++ src/main/java/com/ev/ampora_backend/dto/PaymentRequestDTO.java
@@ -1,4 +1,15 @@
 package com.ev.ampora_backend.dto;
 
+import lombok.Data;
+
 public class PaymentRequestDTO {
+    @Data
+    public class PaymentRequestDTO {
+        private String orderId;
+        private double amount;
+        private String currency;
+        private String customerName;
+        private String customerEmail;
+        private String customerPhone;
+    }
 }
