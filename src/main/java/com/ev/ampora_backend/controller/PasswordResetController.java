1ca8382a5a31276f6f9a5257498bd0d36ef09ab8
Numidu Dulanga <dnumidu@gmail.com>
2026-03-15 13:08:31 +0530

    Change password reset endpoint to '/api/password-reset'

--- src/main/java/com/ev/ampora_backend/controller/PasswordResetController.java
+++ src/main/java/com/ev/ampora_backend/controller/PasswordResetController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/password-reset")
+@RequestMapping("/api/password-reset")
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"http://localhost:5173","https://ampora.dev"})
 public class PasswordResetController {
